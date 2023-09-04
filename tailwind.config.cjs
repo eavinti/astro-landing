74e9d89718794394cad5cd71715b8a6c14c7ec74
Edgar Vintimill <edgar@lugamio.com>
2023-09-04 13:59:46 -0500

    changes hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'roboto': ['Roboto', 'sans-serif'],
+				'nunito': ["'Nunito'", 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
